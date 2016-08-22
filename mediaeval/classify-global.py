6a66351eb7024202a9c9c44de7af8fcac2226fa1
yasolix <yasemin.timar@gmail.com>
2016-08-22 05:32:11 +0300

    update

--- mediaeval/classify-global.py
+++ mediaeval/classify-global.py
@@ -21,20 +21,25 @@ train_id = videolabels['id']
 train_name = videolabels['name']
 train_valrank = videolabels['valenceRank']
 train_arrank = videolabels['arousalRank']
-train_valence = videolabels['valenceValue']
-train_arousal = videolabels['arousalValue']
+train_valence = videolabels['valenceValue'][0:5]
+train_arousal = videolabels['arousalValue'][0:5]
 train_valvar = videolabels['valenceVariance']
 train_arvar = videolabels['arousalVariance']
 
 
-labels = sorted(set(train_labels))
-train_labels = np.array([labels.index(x) for x in train_labels])
+labels = sorted(set(train_valence))
+train_valence = np.array([labels.index(x) for x in train_valence])
+
 train_features = all_k
 train_features = np.array(train_features)
 
 REG_C = 1.0
 vallence_model = SVC(kernel = 'precomputed', C = REG_C, max_iter = 10000)
 vallence_model.fit(train_features, train_valence)
+
+
+labels = sorted(set(train_arousal))
+train_arousal = np.array([labels.index(x) for x in train_arousal])
 
 arousal_model = SVC(kernel = 'precomputed', C = REG_C, max_iter = 10000)
 arousal_model.fit(train_features, train_arousal)
