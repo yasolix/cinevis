c9126a4e65fb05cfdd4444ebf9254d5574b39525
yasemin timar <yasemin.timar@gmail.com>
2016-12-28 00:41:18 +0300

    updated

---
+++ mediaeval/report.py
@@ -0,0 +1,37 @@
+#! /usr/bin/env python
+
+import os
+import sys
+import yaml
+import numpy as np
+
+featuresDir = 'logs/features'
+features = [yaml.load(''.join([l for l in open(os.path.join(featuresDir, logFile)) if not l.startswith('[')])) for logFile in sorted(os.listdir(featuresDir))]
+
+fisher_vectorsDir = 'logs/fisher_vectors'
+fisher_vectors = [yaml.load(open(os.path.join(fisher_vectorsDir, logFile)).read()) for logFile in sorted(os.listdir(fisher_vectorsDir))]
+
+#classification = yaml.load(open('data/classification.txt').read())
+
+print 'Average frame count: %d' % np.mean([y['Frame count'] for y in features])
+print 'Average frame size: %dx%d' % tuple(map(lambda x: np.mean([float(y['Original frame size'].split('x')[x]) for y in features]), range(2)))
+print 'Average descriptor count: %d' % np.mean([y['Calls.ComputeDescriptor'] for y in features])
+print ''
+print 'All fps are reported without taking file reading and writing into account, howevere, video decoding is included.'
+print ''
+print 'Features (%s enabled):' % ', '.join([k for k, v in features[0]['Enabled descriptors'].items() if v])
+print '  Average total fps: %.2f' % np.mean([y['Fps'] for y in features])
+
+print '  Average HOG fps: %.2f' % np.mean([y['Frame count'] / float(0.01 + y['Reading (sec)'] + y['Interp (sec)']['HOG'] + y['IntHist (sec)']['HOG'] + y['Desc (sec)']['HOG']) for y in features])
+print '  Average HOF fps: %.2f' % np.mean([y['Frame count'] / float(0.01 + y['Reading (sec)'] + y['Interp (sec)']['HOFMBH'] + y['IntHist (sec)']['HOF'] + y['Desc (sec)']['HOF']) for y in features])
+print '  Average MBH fps: %.2f' % np.mean([y['Frame count'] / float(0.01 + y['Reading (sec)'] + y['Interp (sec)']['HOFMBH'] + y['IntHist (sec)']['MBH'] + y['Desc (sec)']['MBH']) for y in features])
+print ''
+print 'Fisher vectors (components: %d, s-t grids enabled: %s, knn: %s, second order enabled: %s, FLANN trees: %s, FLANN comparisons: %s):' % (fisher_vectors[0]['10-105']['k'], fisher_vectors[0]['Enable spatio-temporal grids (1x1x1, 1x3x1, 1x1x2)'], fisher_vectors[0].get('K_nn', 'N/A'), fisher_vectors[0]['Enable second order'], fisher_vectors[0].get('FLANN trees', -1), fisher_vectors[0].get('FLANN checks', -1))
+print '  Average total fps: %.2lf' % np.mean([y['Frame count'] / float(z.get('Copying (sec)', 0) + z.get('Flann (sec)', 0) + z.get('Assigning (sec)', 0)) for y, z in zip(features, fisher_vectors)])
+print ''
+#print 'Classification:'
+#for k in sorted(set(classification) - set(['mean'])):
+#	print '  %-15s\t%.4f' % (k, classification[k])
+#print ''
+#print '  mean: %.4f' % classification['mean']
+print 'Regression Results'
---
+++ mediaeval/valence-regression.py
@@ -0,0 +1,101 @@
+#! /usr/bin/env python
+
+import os
+import sys
+import itertools
+import numpy as np
+from sklearn.svm import SVR
+from sklearn.svm import SVC
+from sklearn import svm
+from sklearn.metrics import mean_squared_error
+import matplotlib.pyplot as plt
+#%matplotlib inline
+
+
+#evalDir = "/home/yt/Desktop/cvpr2014/repro/mediaeval/data/dataset/Discrete/annotations/"
+#allClipFile = "allClips.txt"
+#labelFile = "ACCEDEranking.txt"
+#annotations = os.path.join(evalDir,labelFile)
+
+allClipFile = "allClips.txt"
+labelFile = 'ACCEDEranking.txt'
+annotations = os.path.join(sys.argv[1],labelFile)
+
+EVAL_DIR, allClips = sys.argv[1], map(lambda l: l[:-1], open(sys.argv[2]))
+all_k = np.loadtxt(sys.stdin)
+
+#all_k =np.loadtxt("/home/yt/Desktop/cvpr2014/repro/mediaeval/data/kernel.txt")
+
+videolabels = np.genfromtxt(annotations, names=True, delimiter='\t', dtype=None)
+
+clip_id = videolabels['id'] 
+name = videolabels['name']
+valrank = videolabels['valenceRank']
+arrank = videolabels['arousalRank']
+valence = videolabels['valenceValue']
+arousal = videolabels['arousalValue']
+valvar = videolabels['valenceVariance']
+arvar = videolabels['arousalVariance']
+
+features = all_k
+features = np.array(features)
+
+
+def splitindex(x,train_perc):
+    r = np.random.rand(len(x)) < train_perc
+    train = x[r == 1]
+    test = x[r == 0]
+    return train,test
+
+scores = list()
+scores_std = list()
+
+for i in xrange(4):
+	trindex, tsindex = splitindex(clip_id,0.85)
+	
+	X=features[trindex]
+	y=valence[trindex]
+
+	tX=features[tsindex]
+	ty=valence[tsindex]
+###############################################################################
+# Fit regression model
+	svr_rbf = SVR(kernel='rbf', C=1e3, gamma=0.1)
+	svr_lin = SVR(kernel='linear', C=1e3)
+	svr_poly = SVR(kernel='poly', C=1e3, degree=4)
+	y_rbf = svr_rbf.fit(X, y).predict(tX)
+	y_lin = svr_lin.fit(X, y).predict(tX)
+	y_poly = svr_poly.fit(X, y).predict(tX)
+
+        mse_rbf = mean_squared_error(ty, y_rbf)
+        mse_lin = mean_squared_error(ty, y_lin)
+        mse_poly = mean_squared_error(ty, y_poly)
+
+	scores.append(np.mean(mse_rbf))
+	scores_std.append(np.std(mse_rbf))
+
+	print
+
+###############################################################################
+# look at the results
+	X=tsindex
+
+	fig = plt.figure(figsize=(10, 10))
+	plt.scatter(X, ty, c='k', label='data')
+	plt.hold('on')
+	plt.scatter(X, y_rbf, c='g', label='RBF model')
+	plt.scatter(X, y_lin, c='r', label='Linear model')
+	plt.scatter(X, y_poly, c='b', label='Polynomial model')
+
+	plt.plot(X, ty, c='k', label='data')
+	plt.plot(X, y_rbf, c='g', label='RBF model')
+	plt.plot(X, y_lin, c='r', label='Linear model')
+	plt.plot(X, y_poly, c='b', label='Polynomial model')
+	plt.xlabel('data')
+	plt.ylabel('target')
+	plt.title('Support Vector Regression')
+	plt.legend()
+	plt.show()
+
+print np.mean(scores)
+print np.mean(scores_std)
